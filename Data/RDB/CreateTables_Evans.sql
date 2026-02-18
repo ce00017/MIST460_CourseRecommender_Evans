9141d78a50d56dd26fa9cf1de119fcf334ebcbd1
CannonFE <cannon0510@gmail.com>
2026-02-18 17:52:27 -0500

    PreClass on Feb18 Commit

--- Data/RDB/CreateTables_Evans.sql
+++ Data/RDB/CreateTables_Evans.sql
@@ -1,43 +1,166 @@
-use MIST460_RDB_Evans
+use MIST460_RDB_Evans; 
+
+-- Order matters (Why?) -- because of foreign key constraints we have to drop the child tables before the parent tables. 
+IF OBJECT_ID('RegistrationSection')  IS NOT NULL DROP TABLE RegistrationSection;
+IF OBJECT_ID('Registration')         IS NOT NULL DROP TABLE Registration;
+IF OBJECT_ID('Section')              IS NOT NULL DROP TABLE Section;
+IF OBJECT_ID('CoursePrerequisite')   IS NOT NULL DROP TABLE CoursePrerequisite;
+IF OBJECT_ID('Course')               IS NOT NULL DROP TABLE Course;
+IF OBJECT_ID('Instructor')           IS NOT NULL DROP TABLE Instructor;
+IF OBJECT_ID('Student')              IS NOT NULL DROP TABLE Student;
+IF OBJECT_ID('Advisor')              IS NOT NULL DROP TABLE Advisor;
+IF OBJECT_ID('Alum')                 IS NOT NULL DROP TABLE Alum;
+IF OBJECT_ID('Major')                IS NOT NULL DROP TABLE Major;
+IF OBJECT_ID('AppUser')              IS NOT NULL DROP TABLE AppUser;
+
+-- create CoursePrerequisite table DONE
+-- create Registration DONE
+-- create RegistrationSection DONE
+
+
 GO
 
-CREATE table AppUser 
-
-(
-    AppUserID int IDENTITY(1,1) 
-        constraint PK_AppUser primary key,
-    FirstName NVARCHAR(50) NOT NULL,
-    LastName NVARCHAR(50) NOT NULL,
-    Email nvarchar(100) NOT NULL
+CREATE TABLE AppUser (
+    AppUserID       INT IDENTITY(1,1) 
+        CONSTRAINT PK_AppUser PRIMARY KEY,
+    Firstname        NVARCHAR(50)  NOT NULL,
+    Lastname        NVARCHAR(50)  NOT NULL,
+    Email           NVARCHAR(100)  NOT NULL 
         CONSTRAINT UK_AppUser_Email UNIQUE,
-    PhoneNumber nvarchar(20) NULL,
-    PasswordHash varbinary(255) NOT NULL,
-    UserRole nvarchar(20) NOT NULL
-        CONSTRAINT CK_AppUser_UserRole CHECK (UserRole IN ('Student', 'Advisor', 'Alumni'))
+    PhoneNumber     NVARCHAR(20)   NULL,
+    PasswordHash    VARBINARY(256)  NOT NULL,      -- store salted hash
+    UserRole        NVARCHAR(20)   NOT NULL
+        CONSTRAINT CK_AppUser_UserRole CHECK (UserRole IN (N'Student', N'Advisor',N'Alum')
+    )
 );
-
 GO
 
 /*
 alter table AppUser
-    NOCHECK constraint CK_AppUser_UserRole;
+	nocheck constraint CK_AppUser_UserRole;
 
-ALTER TABLE AppUser
-    CHECK CONSTRAINT CK_AppUser_UserRole;
+alter table AppUser
+	check constraint CK_AppUser_UserRole;
 */
 
-create table Student
+CREATE TABLE Student (
+    StudentID               INT 
+        CONSTRAINT PK_Student PRIMARY KEY
+        CONSTRAINT FK_Student_AppUser FOREIGN KEY (StudentID)
+        REFERENCES AppUser(AppUserID),
+    TotalCreditsCompleted   INT NOT NULL
+        CONSTRAINT DF_Student_Credits DEFAULT (0)
+        CONSTRAINT CK_Student_TCC CHECK (TotalCreditsCompleted >= 0),
+    GraduationSemesterYear NVARCHAR(25) not null,
+    OverallGPA decimal(3,2) not null 
+        constraint DF_Student_OverallGPA DEFAULT 0.00,
+    MajorGPA decimal(3,2) not null 
+        constraint DF_Student_MajorGPA DEFAULT 0.00
+);
 
-(
-    StudentID int
-        constraint PK_Student primary key
-        constraint FK_Student_AppUserID foreign key references AppUser(AppUserID),
-    TotalCreditsCompleted int NOT NULL
-        CONSTRAINT DF_Student_TotalCreditsCompleted DEFAULT 0, --Credtis completed += SemesterCredits
-    GraduationSemesterYear NVARCHAR(25) NOT NULL,
-    OverallGPA decimal(3,2) NOT NULL
-        CONSTRAINT DF_Student_OverallGPA DEFAULT 0.00,
-    MajorGPA decimal(3,2) NOT NULL
-        CONSTRAINT DF_Student_MajorGPA DEFAULT 0.00
+GO
+
+CREATE TABLE Advisor (
+    AdvisorID   INT CONSTRAINT PK_Advisor PRIMARY KEY,
+    CONSTRAINT FK_Advisor_AppUser FOREIGN KEY (AdvisorID)
+        REFERENCES AppUser(AppUserID)
 );
 GO
+
+CREATE TABLE Alum (
+    AlumID              INT CONSTRAINT PK_Alum PRIMARY KEY,
+    GraduationSemesterYear      NVARCHAR(25) NOT NULL,
+    CONSTRAINT FK_Alum_AppUser FOREIGN KEY (AlumID)
+        REFERENCES AppUser(AppUserID)
+);
+GO
+
+
+CREATE TABLE Major (
+    MajorID     INT IDENTITY(1,1) CONSTRAINT PK_Major PRIMARY KEY,
+    MajorName   NVARCHAR(200) NOT NULL CONSTRAINT UK_Major_Name UNIQUE
+);
+GO
+
+CREATE TABLE Course (
+    CourseID        INT IDENTITY(1,1) CONSTRAINT PK_Course PRIMARY KEY,
+    SubjectCode     NVARCHAR(10)   NOT NULL,      -- e.g., 'MIST'
+    CourseNumber    NVARCHAR(10)   NOT NULL,      -- e.g., '460'
+    Title           NVARCHAR(200)  NOT NULL,
+    CourseDescription     NVARCHAR(MAX)  NULL,
+    Credits         DECIMAL(4,1)   NOT NULL CONSTRAINT DF_Course_Credits DEFAULT (3.0),
+	Capacity int not null default(0),
+    CONSTRAINT UK_Course_SubjectNumber UNIQUE (SubjectCode, CourseNumber),
+    CONSTRAINT CK_Course_Credits CHECK (Credits > 0 AND Credits <= 12.0)
+);
+GO
+
+--This isn't correct yet, I'm going to review the video again, if your seeing this it means I haven't finished it yet. 
+CREATE table CoursePrerequisite (
+    CoursePrerequisiteID int Identity(1,1) NOT NULL
+        CONSTRAINT PK_CoursePrerequisite PRIMARY KEY,
+    CourseID int NOT NULL
+        CONSTRAINT FK_CoursePrerequisite_CourseID FOREIGN KEY (CourseID)
+        REFERENCES Course(CourseID),
+    PrerequisiteCourseID int NOT NULL
+        CONSTRAINT FK_CoursePrerequisite_PrerequisiteCourseID FOREIGN KEY (PrerequisiteCourseID)
+        REFERENCES Course(CourseID),
+    MinimumGrade decimal(3,2) NOT NULL
+        CONSTRAINT CK_CoursePrerequisite_MinGrade CHECK (MinimumGrade >= 2.0 AND MinimumGrade <= 4.0),
+        CONSTRAINT CK_Not_Same_Course CHECK (CourseID <> PrerequisiteCourseID)
+); 
+GO
+
+create table Instructor (
+    InstructorID int identity(1,1) not null,
+    FirstName nvarchar(50) not null,
+    LastName nvarchar(50) not null,
+    constraint pkInstructor primary key(InstructorID)
+);
+
+go
+
+CREATE TABLE Section (
+    SectionID            INT IDENTITY(1,1) CONSTRAINT PK_Section PRIMARY KEY,
+    CourseID                    INT NOT NULL,
+    InstructorID                INT NOT NULL,
+    CRN                         NCHAR(5) NOT NULL,
+    SectionSemester      NVARCHAR(12) NOT NULL, -- 'Spring','Summer','Fall','Winter'
+    SectionYear          int NOT NULL,
+    SectionNumber               NVARCHAR(10) NULL,
+    RemainingOpenings           INT NOT NULL CONSTRAINT DF_Section_Seats DEFAULT (0),
+    SectionAverageRating DECIMAL(4,2) NOT NULL CONSTRAINT DF_Section_Avg DEFAULT (0.0),
+    CONSTRAINT FK_Section_Course FOREIGN KEY (CourseID)
+        REFERENCES Course(CourseID) ON DELETE CASCADE,
+    CONSTRAINT FK_Section_Instructor FOREIGN KEY (InstructorID)
+        REFERENCES Instructor(InstructorID) ON DELETE NO ACTION,
+    CONSTRAINT CK_Section_Sem CHECK (SectionSemester IN (N'Spring',N'Summer',N'Fall',N'Winter')),
+    CONSTRAINT CK_Section_Seats CHECK (RemainingOpenings >= 0),
+    CONSTRAINT CK_CourseOffering_Avg CHECK (SectionAverageRating >= 0 AND SectionAverageRating <= 5)
+);
+GO
+
+CREATE TABLE Registration (
+    RegistrationID INT IDENTITY(1,1) 
+    CONSTRAINT PK_Registration PRIMARY KEY,
+    StudentID INT NOT NULL,
+    SectionID INT NOT NULL,
+    RegistrationDate DATETIME NOT NULL DEFAULT GETDATE(),
+    CONSTRAINT FK_Registration_Student FOREIGN KEY (StudentID)
+        REFERENCES Student(StudentID),
+    CONSTRAINT FK_Registration_Section FOREIGN KEY (SectionID)
+        REFERENCES Section(SectionID)
+);
+GO
+
+CREATE TABLE RegistrationSection (
+    RegistrationSectionID INT IDENTITY(1,1) 
+    CONSTRAINT PK_RegistrationSection PRIMARY KEY,
+    LetterGrade NCHAR(2) NULL,
+    RegistrationID INT NOT NULL,
+    SectionID INT NOT NULL,
+    CONSTRAINT FK_RegistrationSection_Registration FOREIGN KEY (RegistrationID)
+        REFERENCES Registration(RegistrationID),
+    CONSTRAINT FK_RegistrationSection_Section FOREIGN KEY (SectionID)
+        REFERENCES Section(SectionID)
+);
