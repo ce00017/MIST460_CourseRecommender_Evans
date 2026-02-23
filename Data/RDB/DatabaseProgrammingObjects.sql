a8ee902487612d73cd3fd9ec3ec427e0abe27e59
CannonFE <cannon0510@gmail.com>
2026-02-23 18:40:03 -0500

    Turning queries into Stored procedures

--- Data/RDB/DatabaseProgrammingObjects.sql
+++ Data/RDB/DatabaseProgrammingObjects.sql
@@ -1,15 +1,24 @@
-DECLARE @SectionSemester VARCHAR(20) = NULL,
+CREATE PROCEDURE GetSectionsInSemesterAndYear
+    @SectionSemester NVARCHAR(10) = NULL,
     @SectionYear INT = NULL,
-    @CourseID VARCHAR(10) = NULL;
+    @CourseID VARCHAR(10) = NULL
+    AS
+    BEGIN
 SELECT s.SectionNumber, s.InstructorID, s.SectionSemester, s.SectionYear, s.RemainingOpenings, s.SectionAverageRating,
     c.CourseID, c.Title FROM Section s
     JOIN Course c ON s.CourseID = c.CourseID
     WHERE (@SectionSemester IS NULL OR s.SectionSemester = @SectionSemester)
     AND (@SectionYear IS NULL OR s.SectionYear = @SectionYear);
+
+END;
 GO
 
-DECLARE @CourseID INT = NULL;
+CREATE PROCEDURE GetCoursePrerequisites
+    @CourseID INT = NULL
+    AS
+    BEGIN
 SELECT p.PrerequisiteID, c.Title FROM CoursePrerequisite p
     JOIN Course c ON p.PrerequisiteID = c.CourseID
     WHERE (@CourseID IS NULL OR p.CourseID = @CourseID);
+END;
 GO
