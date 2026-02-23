4ab109be983bd97a5e0f65245c761c95173b3fa2
CannonFE <cannon0510@gmail.com>
2026-02-23 18:05:05 -0500

    Added Scalar variables to run queries

--- Data/RDB/DatabaseProgrammingObjects.sql
+++ Data/RDB/DatabaseProgrammingObjects.sql
@@ -1,9 +1,15 @@
+DECLARE @SectionSemester VARCHAR(20) = NULL,
+    @SectionYear INT = NULL,
+    @CourseID VARCHAR(10) = NULL;
 SELECT s.SectionNumber, s.InstructorID, s.SectionSemester, s.SectionYear, s.RemainingOpenings, s.SectionAverageRating,
     c.CourseID, c.Title FROM Section s
     JOIN Course c ON s.CourseID = c.CourseID
     WHERE (@SectionSemester IS NULL OR s.SectionSemester = @SectionSemester)
     AND (@SectionYear IS NULL OR s.SectionYear = @SectionYear);
+GO
 
+DECLARE @CourseID INT = NULL;
 SELECT p.PrerequisiteCourseID, c.Title FROM CoursePrerequisite p
     JOIN Course c ON p.PrerequisiteCourseID = c.CourseID
     WHERE (@CourseID IS NULL OR p.CourseID = @CourseID);
+GO
